e3920766cf3d26ec470c317b6a12150a779b677f
reliruleru00 <93340772+reliruleru00@users.noreply.github.com>
2024-05-11 02:05:21 +0900

    20240511作業分

--- js/spod_sim.js
+++ js/spod_sim.js
@@ -1,3 +1,20 @@
+class turn_data {
+    constructor() {
+        this.turn_number = 0;
+        this.enemy_debuff = [];
+        this.unit_list = [];
+        this.over_drive = 0;
+    }
+
+    turnProceed() {
+        $.each(this.unit_list, function (index, value) {
+            if (!value.blank) {
+                value.turnProceed();
+            }
+        });
+    }
+
+}
 
 class unit_data {
     constructor() {
@@ -7,9 +24,19 @@ class unit_data {
         this.add_turn = false;
         this.unit = null;
         this.normal_attack_element = 0;
+        this.skill_list = [];
+        this.blank = false;
+    }
+    
+    turnProceed() {
+        this.sp += 2;
+        if (this.place_no <= 2) {
+            // 前衛
+            this.sp += 1;
+        }
     }
 }
-class buff_date {
+class buff_data {
     constructor() {
         this.rest_turn = -1;
         this.effect_size = 0;
@@ -44,30 +71,72 @@ function setEventTrigger() {
 
 /* 戦闘開始処理 */
 function battle_start() {
+    let turn_init = new turn_data();
     let unit_list = [];
+
     // スタイル情報を作成
     $.each(select_style_list, function (index, value) {
-        let  unit = new unit_data();
+        let unit = new unit_data();
         unit.place_no = index;
-        unit.sp += $("#chain_" + index).val();
-        unit.sp.normal_attack_element = $("#bracelet_" + index).val();
-        unit.style = value;
+        if (value) {
+            unit.sp += Number($("#chain_" + index).val());
+            unit.sp.normal_attack_element = $("#bracelet_" + index).val();
+            unit.style = value;
+            unit.skill_list = skill_list.filter(obj =>
+                (obj.chara_id === value.style_info.chara_id || obj.chara_id === 0) &&
+                (obj.style_id === value.style_info.style_id || obj.style_id === 0)
+            );
+        } else {
+            unit.blank = true;
+        }
         unit_list.push(unit);
     });
-    addTurn(unit_list);
+    turn_init.unit_list = unit_list;
+    // 
+    addTurn(turn_init);
 }
 
-function addTurn(unit_list) {
-    let div = $('<div>')
-        .addClass("flex");
-    $.each(unit_list, function (index, value) {
+function addTurn(turn_data) {
+    let header_area = $('<div>').addClass("flex");
+    let enemy = $('<div>').append($('<img>').attr("src", "icon/BtnEventBattleActive.webp").addClass("enemy_icon")).addClass("left");
+    let over_drive = $('<div>').append($('<img>').attr("src", "icon/EffectOverdrive1.webp").addClass("od_icon"))
+                                .append($('<img>').attr("src", "icon/NumberOverdrive1.webp").addClass("od_number"));
+    header_area.addClass("container").append(enemy).append(over_drive);
+    let party_member = $('<div>').addClass("flex");
+    let front_area = $('<div>').addClass("flex");
+    let back_area = $('<div>').addClass("flex");
+    turn_data.turnProceed();
+    $.each(turn_data.unit_list, function (index, value) {
+        let chara_div = $('<div>');
         let img = $('<img>')
             .data("chara_no", index)
-            .addClass("select_style");
+            .addClass("unit_style");
         if (value.style) {
+            let sp = $('<div>').text(value.sp).addClass("unit_sp");
             img.attr("src", "icon/" + value.style.style_info.image_url)
+            chara_div.append(img).append(sp);
+        } else {
+            img.attr("src", "img/cross.png")
+            chara_div.append(img);
         }
-        div.append(img);
+        if (value.place_no < 3) {
+            let skill_select = $('<select>').addClass("unit_skill");
+            $.each(value.skill_list, function (index, value) {
+                let option = $('<option>').text(value.skill_name).val(value.skill_id);
+                skill_select.append(option);
+            });
+            chara_div.addClass("front_area")
+            chara_div.prepend(skill_select);
+            front_area.append(chara_div);
+        } else {
+            let skill_select = $('<select>').addClass("pursuit");
+            let option = $('<option>').text("なし").val(0);
+            skill_select.append(option);
+            chara_div.addClass("back_area");
+            chara_div.prepend(skill_select);
+            back_area.append(chara_div);
+        }
     });
-    $("#turn_area").append(div);
+    party_member.append(front_area).append(back_area)
+    $("#turn_area").append(header_area).append(party_member);
 }

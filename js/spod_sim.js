e427504a2b6ee05015cd16d3fa6bd5322b81593b
reliruleru00 <93340772+reliruleru00@users.noreply.github.com>
2024-05-15 02:58:06 +0900

    スキル情報まで

--- js/spod_sim.js
+++ js/spod_sim.js
@@ -1,5 +1,7 @@
 let last_turn;
 let turn_list = [];
+let physical_name = ["", "斬", "突", "打"] 
+let element_name = ["無", "火", "氷", "雷", "光", "闇"] 
 class turn_data {
     constructor() {
         this.turn_number = 0;
@@ -170,12 +172,24 @@ function addTurn(turn_data, kb_add_turn) {
         let skill_select = $('<select>').addClass("unit_skill");
         let option = $('<option>').text("なし").val(0).addClass("back");
         skill_select.append(option);
-        option = $('<option>').text("追撃").val(-1).addClass("back");
-        skill_select.append(option);
         $.each(value.skill_list, function (index, value) {
-            option = $('<option>').text(value.skill_name).val(value.skill_id).addClass("front");
+            let text = value.skill_name;
+            if (value.skill_name == "通常攻撃") {
+                text += `(${physical_name[value.attack_physical]}・${element_name[0]})`;
+            } else if (value.skill_name == "追撃") {
+                text += `(${physical_name[value.attack_physical]})`;
+            } else if (value.attack_id){
+                text += `(${physical_name[value.attack_physical]}・${element_name[value.attack_element]}/${value.sp_cost})`;
+            } else {
+                text += `(${value.sp_cost})`;
+            }
+            option = $('<option>').text(text)
+                    .val(value.skill_id).addClass(value.skill_name == "追撃" ? "back" : "front");
             skill_select.append(option);
         });
+        if (!value.style) {
+            skill_select.css("visibility", "hidden");
+        }
         chara_div.prepend(skill_select);
 
         if (value.place_no < 3) {
@@ -188,10 +202,11 @@ function addTurn(turn_data, kb_add_turn) {
     });
     party_member.append(front_area).append(back_area)
     let select = $('<select>').addClass("action_select");
-    ["OD発動", "行動開始", "行動後OD"].forEach((element, index) => {
+    ["OD発動", "行動開始", "行動開始+OD"].forEach((element, index) => {
         select.append($('<option>').text(element).val(index));
     });
-    let start = $('<input>').attr("type", "button").val("行動開始").addClass("next_turn");
+    select.val(1);
+    let start = $('<input>').attr("type", "button").val("次ターン").addClass("next_turn");
     let footer_area = $('<div>').append(select).append(start);
     back_area.append(footer_area);
     turn.append(header_area).append(party_member);
@@ -230,20 +245,6 @@ function addUnitEvent(unit_list) {
             // 2回目にクリックされた要素を取得
             var second_click = clicked_element;
 
-            // 各要素の位置を取得
-            var firstPosition = first_click.position();
-            var secondPosition = second_click.position();
-            // クリックした要素を交換
-            first_click.animate({
-                left: secondPosition.left,
-                top: secondPosition.top
-            }, 500);
-            // 要素を交換
-            // first_click.after(second_click.clone());
-            // second_click.after(first_click.clone());
-            // first_click.remove();
-            // second_click.remove();
-
             let first_click_unit_data = getUnitData(unit_list, first_click_index);
             first_click_unit_data.place_no = index;
             unit_data.place_no = first_click_index;
@@ -257,11 +258,29 @@ function addUnitEvent(unit_list) {
                 setFrontOptions(second_click.find("select"));
                 setBackOptions(first_click.find("select"));
             }
+            // 要素を交換
+            swapElements(first_click, second_click)
+
             // 最初にクリックした要素をリセット
             first_click = null;
             first_click_index = -1;
         }
     });
+}
+
+// 要素を交換する関数
+function swapElements(firstElement, secondElement) {
+    const firstSelectVal = firstElement.find("select").val();
+    const secondSelectVal = secondElement.find("select").val();
+
+    const firstClone = firstElement.clone();
+    const secondClone = secondElement.clone();
+
+    firstClone.find("select").val(firstSelectVal);
+    secondClone.find("select").val(secondSelectVal);
+
+    firstElement.replaceWith(secondClone);
+    secondElement.replaceWith(firstClone);
 }
 
 // ユニットデータ取得
@@ -274,7 +293,7 @@ function getUnitData(unit_list, index) {
 function setFrontOptions(select) {
     select.find("option.front").show();
     select.find("option.back").hide();
-    select.find("option:not(.back):first").prop('selected', true);
+    select.find("option.front:first").prop('selected', true);
 }
 function setBackOptions(select) {
     select.find("option.front").hide();

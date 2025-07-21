80b469e77001b4253cacbdb2c11576cc76b3a30a
reliruleru00 <93340772+reliruleru00@users.noreply.github.com>
2025-07-21 13:02:01 +0900

    HP指定

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter  as Router, Routes, Route } from 'react-router-dom';
 import DamageCalc from './pages/DamageCalculation';
 import SpodSim from './pages/SpodSimulation';
 

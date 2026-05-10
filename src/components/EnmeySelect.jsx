e84b5a543ed76c830fc69ddb9f6a7753136c886f
reliruleru00 <93340772+reliruleru00@users.noreply.github.com>
2026-05-11 02:35:31 +0900

    トークン上昇

--- src/components/EnmeySelect.jsx
+++ src/components/EnmeySelect.jsx
@@ -49,7 +49,7 @@ const EnmeySelect = ({ enemyClass, enemySelect, handleChange, isFreeInput }) => 
                     <option value="14">セラフ遭遇戦</option>
                     <option value="7">プリズムバトル</option>
                     <option value="13">イベントプリズム</option>
-                    <option value="8">恒星掃戦線</option>
+                    {/* <option value="8">恒星掃戦線</option> */}
                     <option value="9">イベント隠しボス</option>
                     <option value="10">時の修練場/アリーナ</option>
                     {/* <option value="11">制圧戦</option> */}
